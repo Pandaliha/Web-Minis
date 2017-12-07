55b0b9c8c6195a46d82f6e51630f53a8a729826b
Pandaliha <25638438+Pandaliha@users.noreply.github.com>
2017-12-07 18:49:26 +0100

    Page about Bud Spencer

---
+++ Bud/js/index.js
@@ -0,0 +1,15 @@
+$("body").css("background-image","url(http://www.fg-a.com/wallpapers/white-background-vines.jpg)");
+//$("span").css("color", "#f7fc9f" );
+
+ $("li").css("font-size", "20px");
+$("span").css("color", "#aaa6bf");
+$("span").css("font-weight", "700");
+$("span").css("text-shadow", "1px 1px #000");
+$(".text-primary").css("color", "#aaa6bf");
+$("footer").css("border-radius", "0.7");
+$("a").css("color","#000");
+$("a").css("font-size","23");
+$("a").css("text-decoration","none");
+$("a").css("font-weight","700");
+$("a").css("text-shadow", "1px 1px #fff")
+$(".text-center").css("color", "black");
